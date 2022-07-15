4cec3e9b153a840e81b51bd5a7c2955ea9702bcf
Casper van Veen <96796377+Cas1997@users.noreply.github.com>
2022-07-15 10:40:40 +0200

    No longer needed
    Does it with JSON now

--- physics_dictionary_web/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS acronyms;
-
-CREATE TABLE acronyms (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  acronym TEXT UNIQUE NOT NULL,
-  descr TEXT NOT NULL,
-  webpage TEXT NOT NULL,
-  tags TEXT NOT NULL
-);

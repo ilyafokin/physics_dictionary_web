6a4e071de4c8ac87fdccac2a2043dcc410891d9d
Casper van Veen <96796377+Cas1997@users.noreply.github.com>
2022-07-15 10:39:25 +0200

    Update lookup.py

--- physics_dictionary_web/lookup.py
+++ physics_dictionary_web/lookup.py
@@ -4,33 +4,35 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
-from physics_dictionary_web.db import get_db
+from physics_dictionary_web.db2 import Dictionary
 
 bp = Blueprint('lookup', __name__, url_prefix='/')
+dict = Dictionary()
 
 @bp.route('/', methods=('GET', 'POST'))
 def lookup():
     acronym = None
-    description = None
-    webpage = None
+    dict_tup = ()
+    match = []
+    descriptions = []
+    close_match = False
+    amount = 0
     if request.method == 'POST':
         acronym = request.form['acronym']
-        db = get_db()
         error = None
 
         if not acronym:
             error = 'No search term given'
-        
-        if error is None:
-            entry = db.execute(
-            'SELECT * FROM acronyms WHERE acronym = ?', (acronym,)
-            ).fetchone()
-        
-        if entry is None:
-            error = 'No match found.'
-        else:
-            description = entry['descr']
-            webpage = entry['webpage'] if entry['webpage'] != '' else None
-    print(acronym, description, webpage)
+            
 
-    return render_template('content.html', acronym=acronym, description=description, webpage=webpage)
+        if error is None:
+            dict_tup = dict.find_result(acronym)
+            match = dict_tup[0]
+            close_match = dict_tup[1]
+            descriptions = []
+            for i in match:
+                descriptions.append(dict.description(i))
+            amount = len(match)
+            
+
+    return render_template('content.html', acronym=acronym, match=match, descriptions=descriptions, close_match=close_match, amount=amount)
